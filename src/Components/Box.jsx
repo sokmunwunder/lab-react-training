34f6190640c56625b92a06e9d4a600ff8ddd78f3
Sok Mun Wunder <sokmunwunder@gmail.com>
2021-02-27 17:28:08 +0100

    Iteration 4

--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 const BoxColor = (props) => {
   const { r, g, b } = props;
 
-  //   const rRandomBetween = (min, max) => {
-  //     return Math.floor(Math.random() * (max - min * 1)) + min;
-  //   };
+  const averageLightness =
+    [r, g, b].reduce((total, value) => {
+      return total + value;
+    }, 0) / 3;
+
+  const textColor = averageLightness > 255 / 2 ? 'black' : 'white';
 
   const divStyle = {
     backgroundColor: `rgb(${r}, ${g}, ${b})`,
+    padding: 20,
+    color: textColor,
   };
 
   return (
-    <div style={{ divStyle }}>
+    <div style={divStyle}>
       <span>
         rgb({r},{g},{b})
       </span>
